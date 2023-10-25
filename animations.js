9619622b929b6ea8a04648cdc7531a2d4cadc22b
Vincent Simone <vsimone@protonmail.com>
2023-10-25 14:58:43 -0700

    fixed images, added bg, updated nav

--- animations.js
+++ animations.js
@@ -1,5 +1,15 @@
 function loadPage() {
-    //do something
+    // readyScroll()
 }
+
+// function readyScroll() {
+
+//     const navHeight = $('nav').height();
+//     console.log(navHeight);
+
+//     const offset = $(':target').offset();
+//     const scrollto = offset.top - navHeight; // minus fixed header height
+//     $('html, body').animate({scrollTop:scrollto}, 0);
+// }
 
 $(loadPage);
