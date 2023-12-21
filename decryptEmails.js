ccbbed388f28e9fd98f4182f3c6fd08946bb085d
Vincent Simone <vsimone@protonmail.com>
2023-12-20 23:44:53 -0700

    added obfuscation for email addresses and updated Seattle Shogi Meetup

---
+++ decryptEmails.js
@@ -0,0 +1,20 @@
+function loadPage() {
+    decrypt();
+}
+
+function decrypt() {
+    const clubs = document.getElementsByClassName('club');
+    const clubKeys = Object.keys(clubs);
+
+    clubKeys.forEach(club => {
+        const contactLi = clubs[club].getElementsByClassName('contact')[0];
+        const decryptedEmail = atob(contactLi.id);
+
+        const emailAnchor = contactLi.children[0];
+
+        emailAnchor.setAttribute("href", `mailto:${decryptedEmail}`);
+        emailAnchor.textContent=decryptedEmail;
+    })
+}
+
+$(loadPage);
